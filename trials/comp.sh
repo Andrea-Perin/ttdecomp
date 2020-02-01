9ca0d45616db1acff37856be02e5eb989875c8cd
Andrea-Perin <xinandre@gmail.com>
2020-02-01 17:42:11 +0100

    Removed unused files, allowed objects dir

--- trials/comp.sh
+++
@@ -1,2 +0,0 @@
-gfortran debug_module.f90 tensor_types.f90 mode_n.f90 swap_axis.f90 partinv.f90 proven.f90 -I/home/andrea/Uni/QI/ttdecomp/src -J/home/andrea/Uni/QI/ttdecomp/mods -L/usr/local/lib -llapack -lblas -g -o trial.x
-
