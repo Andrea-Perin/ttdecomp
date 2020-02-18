ae6f9ae6b7af05413fc008c839c6b7bdb9c816bb
carlopolato <carlopolato@gmail.com>
2020-02-18 15:09:29 +0100

    HOSVD and read from generic mode to tensor

--- trials/main.f90
+++ trials/main.f90
@@ -8,30 +8,40 @@ PROGRAM MAIN
   
   IMPLICIT NONE
 
-  INTEGER,ALLOCATABLE::dim(:)
+  INTEGER,ALLOCATABLE::dimm(:)
   REAL*8,ALLOCATABLE::IN(:,:)
+  INTEGER,ALLOCATABLE::vec(:)
   INTEGER::nn,rows,cols,kk,ii
-  TYPE(DTENSOR3)::my_tens
-  OPEN(332,file='../data/mnist_1k.csv',status="old",action="read")
+  TYPE(DTENSOR3)::my_tens,copy
+  !OPEN(332,file='../data/mnist_1k.csv',status="old",action="read")
+  OPEN(332,file='../data/prova.csv',status="old",action="read")
   READ (332, *) nn
-  ALLOCATE(dim(nn))
-  READ(332,*) dim
-  rows=dim(1)
+  ALLOCATE(dimm(nn))
+  READ(332,*) dimm
+  rows=dimm(1)
   cols=1
   DO kk=1,nn-1
-     cols=cols*dim(1+kk)
+     cols=cols*dimm(1+kk)
   END DO
   ALLOCATE(IN(cols,rows))
   READ(332,*) IN
   IN=TRANSPOSE(IN)
   CLOSE(332)
   WRITE (6,*) nn
-  WRITE (6,*) "info:",dim
+  WRITE (6,*) "info:",dimm
   WRITE (6,*) "MATRIX:"
-  CALL TENSOR3(dim,IN,my_tens)
-  DO ii=1,dim(2)
+  CALL TENSOR3(dimm,IN,my_tens,3) ! assuming mode 1
+  DO ii=1,dimm(2)
      WRITE(6,1) my_tens%elems(1,ii,:)
 1    FORMAT (*(F4.0:x)) 
   END DO
-  
+  WRITE (6,*) "dimensioni del tensore"
+  ALLOCATE(vec(3))
+  vec = SHAPE(my_tens%elems)
+  WRITE (6,*) vec
+  ALLOCATE(copy%elems(vec(1),vec(2),vec(3)))
+  copy%elems=my_tens%elems
+  DO ii=1,dimm(2)
+     WRITE(6,1) copy%elems(1,ii,:) 
+  END DO
 END PROGRAM MAIN
