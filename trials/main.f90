f390d0727b67bb9aae6ecf3348512209256eddb6
carlopolato <carlopolato@gmail.com>
2020-02-22 16:53:47 +0100

    minor changes

--- trials/main.f90
+++ trials/main.f90
@@ -15,7 +15,7 @@ PROGRAM MAIN
   REAL*8,ALLOCATABLE::IN(:,:)
   INTEGER,ALLOCATABLE::ranks(:)
   INTEGER::nn,rows,cols,kk,ii
-  TYPE(DTENSOR4)::my_tens,copy,approx,core
+  TYPE(DTENSOR3)::my_tens,copy,approx,core
   !JUST TO TRY...
   TYPE(matrix_list) :: lista(3)
   TYPE(tensor_list) :: tens_lista(3)
@@ -24,8 +24,8 @@ PROGRAM MAIN
   REAL*8 :: threshold, error
   REAL*8, ALLOCATABLE :: lambdas(:)
   !OPEN(332,file='../data/mnist_1k.csv',status="old",action="read")
-  !OPEN(332,file='../data/land_112_240.csv',status="old",action="read")
-  OPEN(332,file='../data/original_144p.csv',status="old",action="read")
+  OPEN(332,file='../data/land_112_240.csv',status="old",action="read")
+  !OPEN(332,file='../data/original_144p.csv',status="old",action="read")
   READ (332, *) nn
   ALLOCATE(dimm(nn))
   READ(332,*) dimm
@@ -38,7 +38,7 @@ PROGRAM MAIN
   READ(332,*) IN
   IN=TRANSPOSE(IN)
   CLOSE(332)
-  my_tens=TENSOR4(dimm,IN,1) ! assuming mode 1
+  my_tens=TENSOR3(dimm,IN,1) ! assuming mode 1
 
   ! !TUCKER DECOMPOSITION
   ! ALLOCATE(ranks(4))
@@ -53,19 +53,19 @@ PROGRAM MAIN
   ! END DO
   ! CLOSE(333)
 
-  !CP DECOMPOSITION
-  rango=16
-  CALL CPD(my_tens,rango,lista,lambdas,error,numiter=2)
-  !RECONSTRUCT
-  core = TENSOR4( (/rango,rango,rango,rango/) , TO_IDENTITY(lambdas,SIZE(my_tens%modes)) , 1 )
-  approx = RECO4(core,lista)
-  !SAVE ON FILE
-  !OPEN(333,file='../data/land_112_240_cpd_10_iter_2.csv',status="unknown",action="write")
-  OPEN(333,file='../data/original_144p_cpd_16_iter_2.csv',status="unknown",action="write")
-  DO ii=1,dimm(1)
-    WRITE(333,*) approx%elems(ii,:,:,:) 
-  END DO
-  CLOSE(333)
+  ! !CP DECOMPOSITION
+  ! rango=16
+  ! CALL CPD(my_tens,rango,lista,lambdas,error,numiter=2)
+  ! !RECONSTRUCT
+  ! core = TENSOR4( (/rango,rango,rango,rango/) , TO_IDENTITY(lambdas,SIZE(my_tens%modes)) , 1 )
+  ! approx = RECO4(core,lista)
+  ! !SAVE ON FILE
+  ! !OPEN(333,file='../data/land_112_240_cpd_10_iter_2.csv',status="unknown",action="write")
+  ! OPEN(333,file='../data/original_144p_cpd_16_iter_2.csv',status="unknown",action="write")
+  ! DO ii=1,dimm(1)
+  !   WRITE(333,*) approx%elems(ii,:,:,:) 
+  ! END DO
+  ! CLOSE(333)
 
 
   !MPS? MAYBE!
